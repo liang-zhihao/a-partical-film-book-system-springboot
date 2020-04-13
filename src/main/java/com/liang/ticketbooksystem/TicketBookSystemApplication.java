7a34bc4d35eeeee3861386860835a1d17761c670
liang <347733121@qq.com>
2020-04-13 22:38:54 +0800

    second commit

--- src/main/java/com/liang/ticketbooksystem/TicketBookSystemApplication.java
+++ src/main/java/com/liang/ticketbooksystem/TicketBookSystemApplication.java
@@ -1,5 +1,6 @@
 package com.liang.ticketbooksystem;
 
+import org.apache.ibatis.io.Resources;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/liang/ticketbooksystem/mapper/UserMapper.java
+++
@@ -1,8 +0,0 @@
-package com.liang.ticketbooksystem.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.liang.ticketbooksystem.service.User;
-
-public interface UserMapper extends BaseMapper<User> {
-
-}
